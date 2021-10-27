b50c4c5e5195cb0229e7d5bd808334635d14fd56
haotongl <haotongl@outlook.com>
2021-10-27 16:53:18 +0800

    update eval dtu

--- eval/eval_mysplit.py
+++ eval/eval_mysplit.py
@@ -203,164 +203,116 @@ with torch.no_grad():
         images = data["images"][0]  # (NV, 3, H, W)
 
         NV, _, H, W = images.shape
+        images_new = []
+        for img_idx in range(NV):
+            image_path = os.path.join('data/dtu/Rectified', f'{obj_name}_train','rect_{:03d}_3_r5000.png'.format(img_idx+1))
+            img = ((np.array(imageio.imread(image_path)) / 255.) * 2. - 1.).astype(np.float32)
+            images_new.append(torch.tensor(img))
+        images_new = torch.stack(images_new)
+        images = images_new.permute(0, 3, 1, 2)
+        NV, _, H, W = images.shape
+        data['focal'] = data['focal'] * 2.
+        data['c'][0][0] = (data['c'][0][0] - 40) * 2.
+        data['c'][0][1] = (data['c'][0][1] - 22) * 2.
 
-        if args.scale != 1.0:
-            Ht = int(H * args.scale)
-            Wt = int(W * args.scale)
-            if abs(Ht / args.scale - H) > 1e-10 or abs(Wt / args.scale - W) > 1e-10:
-                warnings.warn(
-                    "Inexact scaling, please check {} times ({}, {}) is integral".format(
-                        args.scale, H, W
+
+        near_dict = {23: [15, 22, 33], 24: [14, 25, 33], 32: [31, 33, 25], 44: [43, 33, 31]}
+        for k in near_dict:
+            source = torch.Tensor(near_dict[k]).long()
+            all_rays = None
+            if all_rays is None or use_source_lut or args.free_pose:
+                NS = len(source)
+                src_view_mask = torch.zeros(NV, dtype=torch.bool)
+                src_view_mask[source] = 1
+
+                focal = data["focal"][0]
+                if isinstance(focal, float):
+                    focal = torch.tensor(focal, dtype=torch.float32)
+                focal = focal[None]
+
+                c = data.get("c")
+                if c is not None:
+                    c = c[0].to(device=device).unsqueeze(0)
+
+                poses = data["poses"][0]  # (NV, 4, 4)
+                src_poses = poses[src_view_mask].to(device=device)  # (NS, 4, 4)
+
+                target_view_mask = target_view_mask_init.clone()
+                if not args.include_src:
+                    target_view_mask *= ~src_view_mask
+                target = torch.Tensor([k]).long()
+                target_view_mask = torch.zeros(NV, dtype=torch.bool)
+                target_view_mask[target] = 1
+
+                novel_view_idxs = target_view_mask.nonzero(as_tuple=False).reshape(-1)
+
+                poses = poses[target_view_mask]  # (NV[-NS], 4, 4)
+
+                all_rays = (
+                    util.gen_rays(
+                        poses.reshape(-1, 4, 4),
+                        W,
+                        H,
+                        focal * args.scale,
+                        z_near,
+                        z_far,
+                        c=c * args.scale if c is not None else None,
                     )
-                )
-            H, W = Ht, Wt
+                    .reshape(-1, 8)
+                    .to(device=device)
+                )  # ((NV[-NS])*H*W, 8)
 
-        all_rays = None
-        if all_rays is None or use_source_lut or args.free_pose:
-            if use_source_lut:
-                obj_id = cat_name + "/" + obj_basename
-                source = source_lut[obj_id]
-            __import__('ipdb').set_trace()
-            splits = np.load('eval/Splits/{}/split.npy'.format(obj_name), allow_pickle=True).item()
-            source = torch.Tensor(splits['gen']).long()
-            NS = len(source)
-            src_view_mask = torch.zeros(NV, dtype=torch.bool)
-            src_view_mask[source] = 1
+                poses = None
+                focal = focal.to(device=device)
 
-            focal = data["focal"][0]
-            if isinstance(focal, float):
-                focal = torch.tensor(focal, dtype=torch.float32)
-            focal = focal[None]
+        ##########################################
 
-            c = data.get("c")
-            if c is not None:
-                c = c[0].to(device=device).unsqueeze(0)
+            rays_spl = torch.split(all_rays, args.ray_batch_size, dim=0)  # Creates views
 
-            poses = data["poses"][0]  # (NV, 4, 4)
-            src_poses = poses[src_view_mask].to(device=device)  # (NS, 4, 4)
+            n_gen_views = len(novel_view_idxs)
 
-            target_view_mask = target_view_mask_init.clone()
-            if not args.include_src:
-                target_view_mask *= ~src_view_mask
-            target = torch.Tensor(splits['test']).long()
-            target_view_mask = torch.zeros(NV, dtype=torch.bool)
-            target_view_mask[target] = 1
+            net.encode(
+                images[src_view_mask].to(device=device).unsqueeze(0),
+                src_poses.unsqueeze(0),
+                focal,
+                c=c,
+            )
 
-            novel_view_idxs = target_view_mask.nonzero(as_tuple=False).reshape(-1)
+            all_rgb, all_depth = [], []
+            for rays in tqdm.tqdm(rays_spl):
+                rgb, depth = render_par(rays[None])
+                rgb = rgb[0].cpu()
+                depth = depth[0].cpu()
+                all_rgb.append(rgb)
+                all_depth.append(depth)
 
-            poses = poses[target_view_mask]  # (NV[-NS], 4, 4)
+            all_rgb = torch.cat(all_rgb, dim=0)
+            all_depth = torch.cat(all_depth, dim=0)
+            all_depth = (all_depth - z_near) / (z_far - z_near)
+            all_depth = all_depth.reshape(n_gen_views, H, W).numpy()
 
-            all_rays = (
-                util.gen_rays(
-                    poses.reshape(-1, 4, 4),
-                    W,
-                    H,
-                    focal * args.scale,
-                    z_near,
-                    z_far,
-                    c=c * args.scale if c is not None else None,
-                )
-                .reshape(-1, 8)
-                .to(device=device)
-            )  # ((NV[-NS])*H*W, 8)
-
-            poses = None
-            focal = focal.to(device=device)
-
-        rays_spl = torch.split(all_rays, args.ray_batch_size, dim=0)  # Creates views
-
-        n_gen_views = len(novel_view_idxs)
-
-        net.encode(
-            images[src_view_mask].to(device=device).unsqueeze(0),
-            src_poses.unsqueeze(0),
-            focal,
-            c=c,
-        )
-
-        all_rgb, all_depth = [], []
-        for rays in tqdm.tqdm(rays_spl):
-            rgb, depth = render_par(rays[None])
-            rgb = rgb[0].cpu()
-            depth = depth[0].cpu()
-            all_rgb.append(rgb)
-            all_depth.append(depth)
-
-        all_rgb = torch.cat(all_rgb, dim=0)
-        all_depth = torch.cat(all_depth, dim=0)
-        all_depth = (all_depth - z_near) / (z_far - z_near)
-        all_depth = all_depth.reshape(n_gen_views, H, W).numpy()
-
-        all_rgb = torch.clamp(
-            all_rgb.reshape(n_gen_views, H, W, 3), 0.0, 1.0
-        ).numpy()  # (NV-NS, H, W, 3)
-        if has_output:
-            obj_out_dir = os.path.join(output_dir, obj_name)
-            os.makedirs(obj_out_dir, exist_ok=True)
-            for i in range(n_gen_views):
-                out_file = os.path.join(
-                    obj_out_dir, "{:06}.png".format(novel_view_idxs[i].item())
-                )
-                imageio.imwrite(out_file, (all_rgb[i] * 255).astype(np.uint8))
-
-                if args.write_depth:
-                    out_depth_file = os.path.join(
-                        obj_out_dir, "{:06}_depth.exr".format(novel_view_idxs[i].item())
+            all_rgb = torch.clamp(
+                all_rgb.reshape(n_gen_views, H, W, 3), 0.0, 1.0
+            ).numpy()  # (NV-NS, H, W, 3)
+            if has_output:
+                obj_out_dir = os.path.join(output_dir, obj_name)
+                os.makedirs(obj_out_dir, exist_ok=True)
+                for i in range(n_gen_views):
+                    out_file = os.path.join(
+                        obj_out_dir, "{:06}.png".format(novel_view_idxs[i].item())
                     )
-                    out_depth_norm_file = os.path.join(
-                        obj_out_dir,
-                        "{:06}_depth_norm.png".format(novel_view_idxs[i].item()),
-                    )
-                    depth_cmap_norm = util.cmap(all_depth[i])
-                    cv2.imwrite(out_depth_file, all_depth[i])
-                    imageio.imwrite(out_depth_norm_file, depth_cmap_norm)
+                    imageio.imwrite(out_file, (all_rgb[i] * 255).astype(np.uint8))
 
-        curr_ssim = 0.0
-        curr_psnr = 0.0
-        if not args.no_compare_gt:
-            images_0to1 = images * 0.5 + 0.5  # (NV, 3, H, W)
-            images_gt = images_0to1[target_view_mask]
-            rgb_gt_all = (
-                images_gt.permute(0, 2, 3, 1).contiguous().numpy()
-            )  # (NV-NS, H, W, 3)
-            for view_idx in range(n_gen_views):
-                ssim = skimage.measure.compare_ssim(
-                    all_rgb[view_idx],
-                    rgb_gt_all[view_idx],
-                    multichannel=True,
-                    data_range=1,
-                )
-                psnr = skimage.measure.compare_psnr(
-                    all_rgb[view_idx], rgb_gt_all[view_idx], data_range=1
-                )
-                curr_ssim += ssim
-                curr_psnr += psnr
-
-                if args.write_compare:
+            if not args.no_compare_gt:
+                images_0to1 = images * 0.5 + 0.5  # (NV, 3, H, W)
+                images_gt = images_0to1[target_view_mask]
+                rgb_gt_all = (
+                    images_gt.permute(0, 2, 3, 1).contiguous().numpy()
+                )  # (NV-NS, H, W, 3)
+                for view_idx in range(n_gen_views):
                     out_file = os.path.join(
                         obj_out_dir,
                         "{:06}_compare.png".format(novel_view_idxs[view_idx].item()),
                     )
                     out_im = np.hstack((all_rgb[view_idx], rgb_gt_all[view_idx]))
                     imageio.imwrite(out_file, (out_im * 255).astype(np.uint8))
-        curr_psnr /= n_gen_views
-        curr_ssim /= n_gen_views
-        curr_cnt = 1
-        total_psnr += curr_psnr
-        total_ssim += curr_ssim
-        cnt += curr_cnt
-        if not args.no_compare_gt:
-            print(
-                "curr psnr",
-                curr_psnr,
-                "ssim",
-                curr_ssim,
-                "running psnr",
-                total_psnr / cnt,
-                "running ssim",
-                total_ssim / cnt,
-            )
-        finish_file.write(
-            "{} {} {} {}\n".format(obj_name, curr_psnr, curr_ssim, curr_cnt)
-        )
-print("final psnr", total_psnr / cnt, "ssim", total_ssim / cnt)
